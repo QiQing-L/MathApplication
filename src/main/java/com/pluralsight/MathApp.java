44e550e2e55edd7423f991e8eb216687730e6439
Qi Qing <learn.qiqing@gmail.com>
2025-09-24 15:33:02 -0400

    added question 1 answer, trying to figer out how to print the name.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+
+        // step 1:
+        // Question 1:
+        double bobSalary, garySalary;
+        double highestSalary;
+        double Bob, Gary;
+        bobSalary = 50_000;
+        garySalary = 60_000;
+        Bob = bobSalary;
+        Gary = garySalary;
+        highestSalary = Math.max(bobSalary,garySalary);
+
+        System.out.println("The highest salary is " + highestSalary);
+
+    }
+
 }
