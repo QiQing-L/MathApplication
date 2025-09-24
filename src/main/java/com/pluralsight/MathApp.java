5a94e507955f75d276987d3570519fa5386738f5
Qi Qing <learn.qiqing@gmail.com>
2025-09-24 19:11:26 -0400

    completed and tested question 4. Math.sqrt() is used to find square root.

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -7,7 +7,6 @@ public class MathApp {
         // Question 1:
         //Determine whose (Bob or Gary) salary is greater using Math.max()
         // and store the answer in highestSalary.
-
         double bobSalary, garySalary;
         double highestSalary;
         bobSalary = 30_000.90;
@@ -23,7 +22,6 @@ public class MathApp {
         // Question 2:
         //Find and display the smallest of two variables named carPrice and
         //truckPrice. Set the variables to any value you want.
-
         double carPrice, truckPrice;
         carPrice = 80_000;
         truckPrice = 100_000;
@@ -31,14 +29,19 @@ public class MathApp {
         smallestPrice = Math.min(carPrice, truckPrice);
         System.out.println("The smallest price is $" + smallestPrice + ".");
 
-
         //Question 3:
         // Find and display the area of a circle whose radius is 7.25.
-
         double radius = 7.25;
         double radiusSquare = Math.pow(radius,2);
         double area = Math.PI * radiusSquare;
         System.out.println("The area of the circle is " + area + ".");
+
+        // Question 4:
+        //Find and display the square root a variable after it is set to 5.0
+        double number1 = 5.0;
+        double number1SqRoot =Math.sqrt(number1);
+        System.out.println("The square root of " + number1 + " is " + number1SqRoot + ".");
+
 
 
     }
